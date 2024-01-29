d8b201da7c7e69d2d738351342294e3d7d3a7764
Viktor Borisov <borisov_vk@mail.ru>
2024-01-29 13:54:18 +0000

    v1.0 release

--- plot_results.py
+++ plot_results.py
@@ -1,52 +1,62 @@
 import os
-import shutil
+import pandas as pd
+import json
+from plotly import express as px
 import argparse
 
-from mm_det_log_parse import LogParser
-from find_files import find_train_logs, find_test_logs
+
+def get_log(json_path):
+    train_log_list = []
+    val_log_list = []
+
+    with open(json_path, "r", encoding='UTF-8') as f:
+        log_line = f.readline()#log = json.load(f)
+
+        while log_line!="":
+            try:
+                json_log = json.loads(log_line)
+            except Exception as e:
+                print(log_line)
+                print(e)
+            try:
+              #  print(json_log)
+                json_log['loss']
+                # continuation means that it's train
+                train_log_list.append(json_log)
+            except KeyError:
+                # val line
+                val_log_list.append(json_log)
+            log_line = f.readline()
+                
+    return pd.DataFrame(train_log_list), pd.DataFrame(val_log_list)
 
 
-def save_plots(json_path, save_folder, exp_name):
-    save_path = os.path.join(save_folder, exp_name)
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+def save_train_plots(train_df, save_path):
+    fig = px.line(train_df, x="iter", y="loss",)
+    fig.show()
 
-    parser = LogParser(json_path)
-    parser.train_df.to_csv(os.path.join(save_path, f'{exp_name}_train.csv'),)
-    parser.val_df.to_csv(os.path.join(save_path, f'{exp_name}_val.csv'),)
-    shutil.copy(json_path, os.path.join(save_path, 'scalars.json'))
+    fig.write_image(os.path.join(save_path, "loss.png"))
 
 
-def save_test_logs(json_path, save_folder, exp_name):
-    save_path = os.path.join(save_folder, exp_name)
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+def save_val_plots(val_df, save_path):
+    fig = px.line(val_df, x="step", y="coco/bbox_mAP_50")
+    fig.show()
 
-    shutil.copy(json_path, os.path.join(save_path, 'test.json'))
+    fig.write_image(os.path.join(save_path, "mAP50.png"))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='CSV results'
+    parser = argparse.ArgumentParser(description='CSV results' 
                                      'saver for mmdetection logs')
-    parser.add_argument('save_folder')
-    parser.add_argument('experiments_path')
-    parser.add_argument('experiment_name')
+    parser.add_argument('save_path')
+    parser.add_argument('json_path')
 
     params = parser.parse_args()
-    path_to_search = os.path.join(params.experiments_path,
-                                  params.experiment_name)
-    try:
-        train_json_path = find_train_logs(path_to_search)
-        save_plots(train_json_path, params.save_folder, params.experiment_name)
-    except Exception as e:
-        print('Error while searching for train results', e)
 
-    try:
-        test_json_path = find_test_logs(path_to_search)
-        save_test_logs(test_json_path, params.save_folder,
-                       params.experiment_name)
-    except Exception as e:
-        print('Error while searching for train results', e)
-
-
-# python plot_results.py save_folder exps model_exps
+    train_df, val_df = get_log(params.json_path)
+    
+    val_df.to_csv(os.path.join(params.save_path, "val.csv"))
+    train_df.to_csv(os.path.join(params.save_path, "train.csv"))
+    
+    save_train_plots(train_df, params.save_path)
+    save_val_plots(val_df, params.save_path)
